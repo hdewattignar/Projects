4c03e833a02a7323c25c651cf22ba7a175ef42a6
hdewattignar <hdewattignar@gmail.com>
2017-03-23 16:44:44 +1300

    trying to add moving obstacles

---
+++ Unity Projects/Simple Endless Runner/Assets/Scripts/ObstacleMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float sidewaysForce;
+
+    void Start()
+    {
+
+        //Debug.Log("Hello World");        
+    }
+
+	// Update is called once per frame
+	void FixedUpdate() {
+
+        rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+
+        if (rb.position.x > 4 && sidewaysForce > 0)
+        {
+            rb.AddForce(0, 0, 0, ForceMode.Impulse);
+            sidewaysForce = -sidewaysForce;
+            
+        }
+        else if (rb.position.x < -4 && sidewaysForce < 0)
+        {
+            rb.AddForce(0, 0, 0, ForceMode.Impulse);
+            sidewaysForce = -sidewaysForce;
+        }        
+	}
+}
