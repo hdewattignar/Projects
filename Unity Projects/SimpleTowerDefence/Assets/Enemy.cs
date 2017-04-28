535d59a25735ab9c778962ae9a8d27f402b44e71
hdewattignar <hdewattignar@gmail.com>
2017-04-28 13:56:03 +1200

    EnemyBehavior
    enemies follow path

---
+++ Unity Projects/SimpleTowerDefence/Assets/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed;
+
+    private Transform target;
+    private int wayPointIndex = 0;
+
+    void Start()
+    {
+        target = Waypoints.points[0];
+    }
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.1f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+
+    void GetNextWaypoint()
+    {
+        if (wayPointIndex >= Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+            
+
+        wayPointIndex++;
+        target = Waypoints.points[wayPointIndex];
+    }
+}
