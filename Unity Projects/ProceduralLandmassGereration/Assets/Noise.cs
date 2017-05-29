5f6e1a8313b5dff4a23ac0e2e5d2300be321d42a
hdewattignar <hdewattignar@gmail.com>
2017-05-29 14:53:01 +1200

    perlin noise draw to terrain

---
+++ Unity Projects/ProceduralLandmassGereration/Assets/Noise.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Noise {
+
+    public static float[,] GenerateNoiseMap(int width, int height, float scale)
+    {
+
+        float[,] noiseMap = new float[width, height];
+
+        if (scale <= 0)
+        {
+            scale = 0.00001f;
+        }
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                float sampleX = x / scale;
+                float sampleY = y / scale;
+
+                float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
+                noiseMap[x, y] = perlinValue;
+            }
+        }
+
+        return noiseMap;
+
+    }
+	
+}
