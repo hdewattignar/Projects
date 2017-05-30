b545fb448a0f429121351ec1893dfa94c5fe7bc8
hdewattignar <hdewattignar@gmail.com>
2017-05-30 19:01:58 +1200

    Main Menu

---
+++ Unity Projects/SimpleTowerDefence/Assets/Scipts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public string levelToLoad = "Scene_001";
+
+    public void Play()
+    {
+        Debug.Log("Play");
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Exiting");
+        Application.Quit();
+    }
+}
