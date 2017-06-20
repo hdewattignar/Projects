59536dde351601f68015e8ab7bdbc33123cc4dac
hdewattignar <hdewattignar@gmail.com>
2017-06-20 15:04:59 +1200

    controller support test

---
+++ Unity Projects/ControllerSupportTest/Assets/Scripts/Button.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+    public enum InputTypes {Button, LeftStick, RightStick};
+
+    public Material inActiveMat;
+    public Material activeMat;
+
+    public InputTypes input;
+    public string name;
+
+    Vector3 startPos;
+    	
+    void Start()
+    {
+        startPos = this.transform.position;
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+
+        if (input == InputTypes.Button)
+        {
+            if (Input.GetButton(name))
+            {
+                this.transform.GetComponent<Renderer>().material = activeMat;
+            }
+            else
+            {
+                this.transform.GetComponent<Renderer>().material = inActiveMat;
+            }
+        }
+        else
+        {
+            if (input == InputTypes.LeftStick)
+            {
+                Vector3 leftStickMovement = Vector3.zero;
+                leftStickMovement.x = Input.GetAxis("LeftStickHorizontal");
+                leftStickMovement.z = Input.GetAxis("LeftStickVertical");
+                this.transform.position = startPos + leftStickMovement;
+            }
+            else
+            {
+                Vector3 rightStickMovement = Vector3.zero;
+                rightStickMovement.x = Input.GetAxis("RightStickHorizontal");
+                rightStickMovement.z = Input.GetAxis("RightStickVertical");
+                this.transform.position = startPos + rightStickMovement;
+            }
+        }
+
+        
+	}
+}
---
+++ Unity Projects/ControllerSupportTest/Assets/Scripts/ButtonActivate.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonActivate : MonoBehaviour{
+
+    public Material activeMaterial;
+    public Material inActiveMaterial;
+
+    public GameObject Abutton;
+    public GameObject Bbutton;
+    public GameObject Xbutton;
+    public GameObject Ybutton;
+    public GameObject Startbutton;
+    public GameObject Backbutton;
+
+    public GameObject RightStick;
+    public GameObject LeftStick;	
+	
+	// Update is called once per frame
+    //void Update () {
+
+    //    //Buttons
+
+    //    // A button
+    //    if (Input.GetButton("A_button"))
+    //    {
+    //        Abutton.GetComponent<Renderer>().material = activeMaterial;
+    //    }
+    //    else
+    //    {
+    //        Abutton.GetComponent<Renderer>().material = inActiveMaterial;
+    //    }
+
+    //    // B Button
+    //    if (Input.GetButton("B_button"))
+    //    {
+    //        Bbutton.GetComponent<Renderer>().material = activeMaterial;
+    //    }
+    //    else
+    //    {
+    //        Bbutton.GetComponent<Renderer>().material = inActiveMaterial;
+    //    }
+
+    //    // X button
+    //    if (Input.GetButton("X_button"))
+    //    {
+    //        Xbutton.GetComponent<Renderer>().material = activeMaterial;
+    //    }
+    //    else
+    //    {
+    //        Xbutton.GetComponent<Renderer>().material = inActiveMaterial;
+    //    }
+
+    //    // Y button
+    //    if (Input.GetButton("Y_button"))
+    //    {
+    //        Ybutton.GetComponent<Renderer>().material = activeMaterial;
+    //    }
+    //    else
+    //    {
+    //        Ybutton.GetComponent<Renderer>().material = inActiveMaterial;
+    //    }
+
+    //    // Start button
+    //    if (Input.GetButton("Start_button"))
+    //    {
+    //        Startbutton.GetComponent<Renderer>().material = activeMaterial;
+    //    }
+    //    else
+    //    {
+    //        Startbutton.GetComponent<Renderer>().material = inActiveMaterial;
+    //    }
+
+    //    // Back button
+    //    if (Input.GetButton("Back_button"))
+    //    {
+    //        Backbutton.GetComponent<Renderer>().material = activeMaterial;
+    //    }
+    //    else
+    //    {
+    //        Backbutton.GetComponent<Renderer>().material = inActiveMaterial;
+    //    }
+
+    //    //Sticks
+
+    //    //Right
+    //    Vector3 rightStickMovement = Vector3.zero;
+    //    rightStickMovement.x = Input.GetAxis("RightStickHorizontal");
+    //    rightStickMovement.z = Input.GetAxis("RightStickVertical");
+    //    RightStick.transform.position += rightStickMovement;
+
+    //    //Left
+    //    Vector3 leftStickMovement = Vector3.zero;
+    //    leftStickMovement.x = Input.GetAxis("LeftStickHorizontal");
+    //    leftStickMovement.z = Input.GetAxis("LeftStickVertical");
+    //    LeftStick.transform.position += leftStickMovement;
+
+    //}
+}
