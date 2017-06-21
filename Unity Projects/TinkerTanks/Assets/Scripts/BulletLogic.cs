890ba9c220db442528a16c9ad1cc2a03ef720dfb
hdewattignar <hdewattignar@gmail.com>
2017-06-21 22:44:14 +1200

    player movement, turret rotation and firing

---
+++ Unity Projects/TinkerTanks/Assets/Scripts/BulletLogic.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletLogic : MonoBehaviour {
+
+    public float bulletSpeed = 1;
+    public float damage = 50;
+    public float lifeSpan = 5;
+
+    void Start()
+    {
+        this.transform.parent = null;
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        //move bullet
+        this.transform.Translate(0, 0, bulletSpeed * Time.deltaTime, Space.Self);
+
+        //kill bullet after lifeSpan is up
+        lifeSpan -= Time.deltaTime;
+
+        if (lifeSpan < 0)
+        {
+            Destroy(gameObject);
+        }
+
+	}
+}
---
+++ Unity Projects/TinkerTanks/Assets/Scripts/InputManager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+    public GameObject tankInputDirection;
+    public GameObject turretInputDirection;
+    public Transform partToRotate;
+    public Transform firePoint;
+    public GameObject bulletPreFab;
+
+    public float bulletCoolDown = 1;
+    public float turnSpeed = 10f;
+    public float moveSpeed = 10f;
+
+    int stickOffset = 15; //adds more distance to the stick values for better
+
+		
+	// Update is called once per frame
+	void Update () {
+
+        GetControlInputs();
+
+        if (bulletCoolDown < 1)
+        {
+            bulletCoolDown += Time.deltaTime;
+        }
+
+	}
+
+    void GetControlInputs()
+    {
+        //Tank rotation
+        Vector3 rightStickRotation = Vector3.zero;
+        rightStickRotation.x = Input.GetAxis("RightStickHorizontal");
+        rightStickRotation.z = Input.GetAxis("RightStickVertical");
+        tankInputDirection.transform.position = this.transform.position + (new Vector3(rightStickRotation.x * stickOffset, 0, rightStickRotation.z * stickOffset));
+
+        //Turret rotation
+        Vector3 leftStickRotation = Vector3.zero;
+        leftStickRotation.x = Input.GetAxis("LeftStickHorizontal");
+        leftStickRotation.z = Input.GetAxis("LeftStickVertical");
+        turretInputDirection.transform.position = partToRotate.transform.position + (new Vector3(leftStickRotation.x * stickOffset, 0, leftStickRotation.z * stickOffset));
+
+        CalculateRotation();
+
+        //drive forward        
+        //Debug.Log(Input.GetAxis("RightTrigger") + "right");
+        //Debug.Log(Input.GetAxis("LeftTrigger") + "left");
+        this.transform.Translate(Vector3.forward * (moveSpeed * -Input.GetAxis("RightTrigger")), Space.Self);
+
+        //drive back
+        this.transform.Translate(Vector3.back * (moveSpeed * Input.GetAxis("LeftTrigger")), Space.Self);
+
+        //fire
+        if (Input.GetButton("LeftShoulderButton") && bulletCoolDown >= 1)
+        {
+            FireTurret();
+        }
+    }
+
+    void CalculateRotation()
+    {
+
+        //check input direction is not zero
+        if (tankInputDirection.transform.position != this.transform.position)
+        {
+            Vector3 dir = tankInputDirection.transform.position - this.transform.position;
+            Quaternion lookRotation = Quaternion.LookRotation(dir);
+            Vector3 rotation = (Quaternion.Lerp(this.transform.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles);
+            this.transform.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+        }
+
+        if (turretInputDirection.transform.position != this.transform.position)
+        {
+            Vector3 dir = turretInputDirection.transform.position - this.transform.position;
+            Quaternion lookRotation = Quaternion.LookRotation(dir);
+            Vector3 rotation = (Quaternion.Lerp(partToRotate.transform.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles);
+            partToRotate.transform.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+        }        
+    }
+
+    void FireTurret()
+    {
+        GameObject bulletGO = (GameObject)Instantiate(bulletPreFab, firePoint.position, firePoint.rotation);
+        bulletCoolDown = 0;
+    }
+}
