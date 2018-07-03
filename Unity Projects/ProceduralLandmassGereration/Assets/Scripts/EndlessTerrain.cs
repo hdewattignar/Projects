10242c4a23008e13999c194718b9de2ad3091f0c
hdewattignar <hdewattignar@gmail.com>
2018-07-03 21:36:23 +1200

    endlessterrain finished ep6

---
+++ Unity Projects/ProceduralLandmassGereration/Assets/Scripts/EndlessTerrain.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndlessTerrain : MonoBehaviour {
+
+	public const float macViewDist = 300;
+	public Transform viewer;
+
+	public static Vector2 viewerPosition;
+	int chunksize;
+	int chunksVisableInViewDist;
+
+	void Start(){
+	
+	}
+
+
+}
--- Unity Projects/ProceduralLandmassGereration/Assets/Scripts/MapGenerator.cs
+++ Unity Projects/ProceduralLandmassGereration/Assets/Scripts/MapGenerator.cs
@@ -7,8 +7,9 @@ public class MapGenerator : MonoBehaviour {
     public enum DrawMode { NoiseMap, ColourMap, Mesh };
     public DrawMode drawMode;
 
-    public int mapWidth;
-    public int mapHeight;
+	const int mapChunkSize = 241;    
+	[Range(0,6)] 
+	public int levelOfDetail;
     public float noiseScale;    
 
     public int octaves;
@@ -19,11 +20,34 @@ public class MapGenerator : MonoBehaviour {
     public int seed;
     public Vector2 offset;
 
+	public float meshHeightMultiplier;
+	public AnimationCurve meshHeightCurve;
+
     public bool autoUpdate;
 
     public TerrainType[] regions;
 
-    public void GenerateMap()
+    public void DrawMapInEditor()
+    {
+        MapData mapData = GenerateMap();
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+
+        if (drawMode == DrawMode.NoiseMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(mapData.noiseMap));
+        }
+        else if (drawMode == DrawMode.ColourMap)
+        {
+			display.DrawTexture(TextureGenerator.TextureFromColourMap(mapData.colourMap, mapWidth, mapHeight));
+        }
+        else if (drawMode == DrawMode.Mesh)
+        {
+			display.DrawMesh(MeshGenerator.GenerateTerrainMesh(mapData.noiseMap, meshHeightMultiplier, meshHeightCurve, levelOfDetail), TextureGenerator.TextureFromColourMap(mapData.colourMap, mapWidth, mapHeight));
+        }
+    }
+
+    public MapData GenerateMap()
     {
         float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
@@ -46,36 +70,13 @@ public class MapGenerator : MonoBehaviour {
             }
         }
 
-        MapDisplay display = FindObjectOfType<MapDisplay>();
-
-        if (drawMode == DrawMode.NoiseMap)
-        {
-            display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
-        }
-        else if (drawMode == DrawMode.ColourMap)
-        {
-            display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
-        }
-        else if (drawMode == DrawMode.Mesh)
-        {
-            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap), TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
-        }
-
-        
+		return new MapData (noiseMap, colourMap);
 
     }
 
     void OnValidate()
-    {
-        if (mapWidth < 1)
-        {
-            mapWidth = 1;
-        }
-
-        if (mapHeight < 1)
-        {
-            mapHeight = 1;
-        }
+    {             
+		
         if (lacunarity < 1)
         {
             lacunarity = 1;
@@ -94,4 +95,17 @@ public class MapGenerator : MonoBehaviour {
         public Color colour;
     }
 
+}
+
+
+public struct MapData
+{
+    public float[,] heightMap;
+    public Color[] colourMap;
+
+    public MapData(float[,] heightMap, Color[] colourMap)
+    {
+        this.heightMap = heightMap;
+        this.colourMap = colourMap;
+    }
 }
